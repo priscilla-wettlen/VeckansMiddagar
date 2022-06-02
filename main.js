f8483f31ff83c3a51298e3b7d73745653c8888b1
Priscilla Silva <priscilla.sg91@gmail.com>
2022-06-02 18:22:53 +0200

    need to fix so that dinenrs dont appear 5 times

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 let myDinners = ['Köttfarsås', 'Pulled Pork Nachos', 'Pozole', 'Lasagna', 'Köttfarslimpa',
   'Birria', 'Fisk + Pizza', 'Falafel']
 
-  console.log(myDinners.length)
+//console.log(myDinners.length)
+  
+
+    
+const main = document.querySelector("main")
+const middagar = document.querySelector(".dinners")
+
+main.appendChild(middagar)
+
+
+const shuffled = myDinners.sort(() => 0.5 - Math.random());
+    let selected = shuffled.slice(0, 5);
+
+function displayDinners() {
+  for (let i in selected) {
+    const para = document.createElement("p", ".para")
+    middagar.appendChild(para)
+    para.innerHTML += selected;
+  }
+}
+displayDinners()
