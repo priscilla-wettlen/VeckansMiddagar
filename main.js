356b29631aef60cd7f1a0471a8e43c79fb6a652e
Priscilla Silva <priscilla.sg91@gmail.com>
2022-06-28 01:04:36 +0200

    fixed random and display

--- main.js
+++ main.js
@@ -1,24 +1,83 @@
-let myDinners = ['Köttfarsås', 'Pulled Pork Nachos', 'Pozole', 'Lasagna', 'Köttfarslimpa',
-  'Birria', 'Fisk + Pizza', 'Falafel']
+// let myDinners = ['Köttfarsås', 'Pulled Pork Nachos', 'Pozole', 'Lasagna', 'Köttfarslimpa',
+//   'Birria', 'Fisk + Pizza', 'Falafel']
 
-//console.log(myDinners.length)
+
   
+let myDinners = {
+  kfsas: {
+    name: "Köttfarssås",
+    veg: false,
+    quick: false,
+    img: "img/kfsas.jpeg"
+  },
+  ppnachos: {
+    name: "Pulled Pork Nachos",
+    veg: false,
+    quick: true,
+    img: "img/ppnachos.png"
+  },
+  pozole: {
+    name: "Pozole",
+    veg: false,
+    quick: false,
+    img: "img/pozole.jpeg"
+  },
+  lasagne: {
+    name: "Lasagne",
+    veg: false,
+    quick: false,
+    img: "img/lasagne.jpeg"
+  },
+  kflimpa: {
+    name: "Köttfarslimpa",
+    veg: false,
+    quick: false,
+    img: "img/kflimpa.jpeg"
+  },
+  birria: {
+    name: "Birria",
+    veg: false,
+    quick: false,
+    img: "img/birria.jpeg"
+  },
+  fiskpizza: {
+    name: "Fisk(F) and Pizza(P)",
+    veg: false,
+    quick: true,
+    img: "img/fiskpizza.jpeg"
+  },
+  falafel: {
+    name: "Falafel",
+    veg: true,
+    quick: true,
+    img: "img/falafel.jpeg"
+  },
+  alfredo: {
+    name: "Pasta Alfredo",
+    veg: true,
+    quick: true,
+    img: "img/alfredo.jpeg"
+  }
+}
 
+const values = Object.values(myDinners)
+console.log(values)
+
+const shuffled = values.sort(() => 0.5 - Math.random());
+let selected = shuffled.slice(0, 5);
     
-const main = document.querySelector("main")
-const middagar = document.querySelector(".dinners")
-
-main.appendChild(middagar)
-
-
-const shuffled = myDinners.sort(() => 0.5 - Math.random());
-    let selected = shuffled.slice(0, 5);
-
 function displayDinners() {
-  for (let i in selected) {
+  for (let value in selected) {
+    const main = document.querySelector("main");
+    const dinners = document.querySelector(".dinners");
+    const ul = document.querySelector(".ul");
+    const li = document.createElement("li");
     const para = document.createElement("p", ".para")
-    middagar.appendChild(para)
-    para.innerHTML += selected;
+    main.appendChild(dinners)
+    dinners.appendChild(ul)
+    ul.appendChild(li);
+    li.appendChild(para)
+    para.innerHTML = para.innerHTML += selected[value].name
   }
 }
 displayDinners()
